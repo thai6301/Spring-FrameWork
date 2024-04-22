1367aa85e5122f59bdcb63ae7d7cd85c2c0c82a4
thai6301 <thai632001@gmail.com>
2024-04-22 20:56:33 +0700

    thai 2

---
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/examples/a1/DepInjectionLauncherApplication2.java
@@ -0,0 +1,32 @@
+package com.in28minutes.learnspringframework.examples.a1;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+
+@Configuration
+@ComponentScan
+
+
+public class DepInjectionLauncherApplication2 {
+	
+
+
+	public static void main(String[] args) {
+		
+		try (var context = new AnnotationConfigApplicationContext(DepInjectionLauncherApplication2.class)) {
+			
+			Arrays.stream(context.getBeanDefinitionNames())
+				.forEach(System.out::println);
+		
+		}
+		
+		
+			
+	}
+
+}
---
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/game/MarioGame.java
@@ -0,0 +1,26 @@
+package com.in28minutes.learnspringframework.game;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+@Primary
+public class MarioGame implements GamingConsole {
+	
+	public void up() {
+		System.out.println("Jump");
+	}
+	
+	public void down() {
+		System.out.println("Go into a hole");
+	}
+	
+	public void left() {
+		System.out.println("Go back");
+	}
+	
+	public void right() {
+		System.out.println("Accelerate");
+	}
+
+}
